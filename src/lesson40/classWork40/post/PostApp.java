94f164bb3461290a488417dffd2a11e6a0ecef06
Alexander-Anischenko <156853009+Alexander-Anischenko@users.noreply.github.com>
2024-06-25 15:51:48 +0200

    lesson40.classWork40_Set_and

---
+++ src/lesson40/classWork40/post/PostApp.java
@@ -0,0 +1,40 @@
+package lesson40.classWork40.post;
+//**Задача:** Написать приложение, которое позволит понять,
+// из каких мест на данную почту приходят письма.
+
+import java.util.HashSet;
+import java.util.Scanner;
+import java.util.Set;
+
+public class PostApp {
+    public static void main(String[] args) {
+
+        Set<String> postCode = new HashSet<>();
+
+        String newPostCode;
+
+        System.out.println("Welcome to post Qffice!");
+        Scanner scanner = new Scanner(System.in);
+
+        do {
+            System.out.println("Input next Post Code: ");
+            newPostCode = scanner.nextLine();
+            if (postCode.add(newPostCode)) {
+                System.out.println("Post Code added.");
+            } else {
+                System.out.println("Post Code already exists.");
+            }
+            System.out.println("Continue? y/n");
+            String choice = scanner.nextLine();
+            if (choice .equals("n")) {
+                System.out.println("Good bye!");
+                break;
+            }
+        } while (true);
+
+        System.out.println("Post codes: ");
+        System.out.println(postCode);
+
+
+    }
+}
